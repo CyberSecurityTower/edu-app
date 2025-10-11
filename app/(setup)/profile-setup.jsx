7b41cb6fd1f0442b0f601c48cf25d6f75f582746
CyberSecurityTower <159848727+CyberSecurityTower@users.noreply.github.com>
2025-10-11 20:22:38 +0100

    Auto backup Sat 10/11/2025 20:22:38.21

--- app/(setup)/profile-setup.jsx
+++ app/(setup)/profile-setup.jsx
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet, ActivityIndicator, ScrollView, Pressable } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
